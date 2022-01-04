cf6a65450791f4dfcdc412f8a11932b89f144fe4
sreehanrm2009 <83212024+sreehanrm2009@users.noreply.github.com>
2022-01-04 17:35:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
-function preload() {
+// Variables
+noseX = 0;
+noseY = 0;
 
+// P5.js functions
+function preload() {
+      clown_nose = loadImage('https://i.postimg.cc/63M6390C/nose.png');
 }
 
 function setup() {
-      canvas = createCanvas(640, 480);
-      canvas.position(110, 250);
+      canvas = createCanvas(300, 300);
+      canvas.center();
       video = createCapture(VIDEO);
+      video.size(300, 300);
       video.hide();
-      tint_color = "";
+      poseNet = ml5.poseNet(video, modelLoaded);
+      poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-      image(video, 0, 0, 640, 480);
-      tint(tint_color);
+      image(video, 0, 0, 300, 300);
+      image(clown_nose, noseX, noseY, 30, 30);
 }
+
+// Other functions
 
 function take_snapshot() {
-      save('student_name.png');
+      save('myFilterImage.png');
 }
 
-function filter_tint() {
-      tint_color = document.getElementById("color_name").value;
+function modelLoaded() {
+      console.log('PoseNet is Initialized!');
+}
+
+function gotPoses(results) {
+      if (results.length > 0) {
+            console.log(results);
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("Nose X = " + noseX);
+            console.log("Nose Y = " + noseY);
+      }
 }
